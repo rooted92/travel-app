592bac198eb7aa2ca45e8a7213e7358ad309e087
Pedro Castaneda <petecast76@gmail.com>
2024-10-21 07:38:25 -0700

    feat: created navbar and footer components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-bold text-teal-700">Hello World</h1>
+    <h1 className="font-bold text-teal-500 text-2xl">Hello World</h1>
   );
 }
