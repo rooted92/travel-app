7c49292b3698c8cd0cfb191267322ae865fa2d51
Pedro Castaneda <petecast76@gmail.com>
2024-10-21 12:55:24 -0700

    feat: imported navbar and footer components to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        <main>
+        <main className="relative overflow-hidden">
           {children}
         </main>
         <Footer />
