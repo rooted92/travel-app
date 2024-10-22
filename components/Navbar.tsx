5a7a65d79eef6f96bb245f585887925712aaa603
Pedro Castaneda <petecast76@gmail.com>
2024-10-22 11:31:49 -0700

    imported public folder with new assets

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,19 @@
+import { NAV_LINKS } from "@/constants"
+import Image from "next/image"
 import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href={"./"}>
-        
+        <Image src={"/hilink-logo.svg"} alt="logo" width={74} height={29} />
+
+        <ul className="hidden h-full gap-12 lg:flex">
+          {NAV_LINKS.map((link) => (
+            <Link  href={link.href} key={link.key} />
+            {link.label}
+          ))}
+        </ul>
       </Link>
     </nav>
   )
