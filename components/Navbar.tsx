63c87202c0f06d1557f6013b9f847d4afbd8dc90
Pedro Castaneda <petecast76@gmail.com>
2024-10-23 10:45:19 -0700

    feat: mapped over link array to display links in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,15 +7,14 @@ const Navbar = () => {
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href={"./"}>
         <Image src={"/hilink-logo.svg"} alt="logo" width={74} height={29} />
-
-        <ul className="hidden h-full gap-12 lg:flex">
-          {NAV_LINKS.map((link) => (
-            <Link href={link.href} key={link.key}>
-              {link.label}
-            </Link>
-          ))}
-        </ul>
       </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link href={link.href} key={link.key}>
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
   )
 }
