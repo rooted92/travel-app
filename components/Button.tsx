d7daa28df866aca734f3579d79a8ffa6fe216ee1
Pedro Castaneda <petecast76@gmail.com>
2024-10-23 11:27:05 -0700

    feat: styled and completed login button, finished navbar

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Button = () => {
+type ButtonProps = {
+    type: 'button' | 'submit' | 'reset',
+    title: string,
+    icon?: string,
+    variant: 'btn_dark_green'
+}
+
+const Button = ({type, title, icon, variant}: ButtonProps) => {
   return (
-    <div>button</div>
+    <button className={`flexCenter gap-3 rounded-full border ${variant}`} type={type}>
+        {icon && <Image src={icon} alt={title} width={24} height={24} />}
+        <label className='bold-16 whitespace-nowrap'>{title}</label>
+    </button>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
-import Button from "./Button"
+import Button from "./button"
 
 const Navbar = () => {
   return (
@@ -18,8 +18,20 @@ const Navbar = () => {
         ))}
       </ul>
       <div className="lg:flexCenter hidden">
-        <Button></Button>
+        <Button
+          type="button"
+          title="Login"
+          icon='/user.svg'
+          variant="btn_dark_green"
+         />
       </div>
+      <Image
+        src={"menu.svg"}
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block lg:hidden cursor-pointer"
+      />
     </nav>
   )
 }
