90ee9ea4ae7eb84711f1b765cccc40cdf84f4fdf
Pedro Castaneda <petecast76@gmail.com>
2024-10-23 13:08:54 -0700

    feat: created component files for other sections of the page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
-import Button from "./button"
+import Button from "./Button"
 
 const Navbar = () => {
   return (
