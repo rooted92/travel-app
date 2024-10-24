23ae00166ef8af50b70dcfd4e958ec6dbaa3e020
Pedro Castaneda <petecast76@gmail.com>
2024-10-23 23:36:10 -0700

    created interface for camp props

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,10 +1,77 @@
+import { PEOPLE_URL } from '@/constants'
+import Image from 'next/image'
 import React from 'react'
+
+interface CampSiteProps {
+  backgroundImage?: string,
+  title?: string,
+  subtitle?: string,
+  peopleJoined?: string
+}
+
+const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }) => {
+  return (
+    <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
+      <div className='flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10'>
+        <div className='flexCenter gap-4'>
+
+          <div className='rounded-full bg-green-50 p-4'>
+            <Image
+              src={'/folded-map.svg'}
+              alt='map'
+              width={28}
+              height={28}
+            />
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <h4 className='bold-18 text-white'>{title}</h4>
+            <p className='regular-14 text-white'>{subtitle}</p>
+          </div>
+
+          <div className='flexCenter gap-6'>
+            <span className='flex -space-x-4 overflow-hidden'>
+              {PEOPLE_URL.map((url, index) => (
+                <Image
+                  className='inline-block h-10 w-10 rounded-full'
+                  key={index}
+                  src={url}
+                  alt='people'
+                  width={24}
+                  height={24}
+                />
+              ))}
+            </span>
+            <p className='bold-16 md:bold-20 text-white'>{peopleJoined}</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const Camp = () => {
   return (
-    <section className='border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
+    <section className='2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
       <div className='hide-scrollbar flex h-[-340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]'>
-        
+        <CampSite
+          backgroundImage='bg-bg-img-1'
+          title='Putuk Truno Camp'
+          subtitle='Malang, Indonesia'
+          peopleJoined='50+ Joined'
+        />
+        <CampSite
+          backgroundImage='bg-bg-img-2'
+          title='Mountain View'
+          subtitle='Somewhere in the wilderness'
+          peopleJoined='50+ Joined'
+        />
+      </div>
+      <div className='flex-end mt-10 px-6 lg:-mt-60 lg:mr-6'>
+        <div className='bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-4 overflow-hidden rounded-3xl'>
+          <h2>Feeling Lost And Not Knowing</h2>
+        </div>
       </div>
     </section>
   )
