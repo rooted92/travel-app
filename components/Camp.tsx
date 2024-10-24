1ec8041728bc3befaaf6fc44528f36f4ab77689a
Pedro Castaneda <petecast76@gmail.com>
2024-10-24 10:48:24 -0700

    fix: added name of interface to the parameter list in CampSite component

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -9,7 +9,7 @@ interface CampSiteProps {
   peopleJoined?: string
 }
 
-const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }) => {
+const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampSiteProps) => {
   return (
     <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
       <div className='flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10'>
